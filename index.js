93ac3447d7805aa077572666c73da01e5b5515cb
Paul Stone Ero <eropaul@gmail.com>
2020-01-05 22:41:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
-}
-
-happyHolidaysTo("Paul")
 
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holidays}, ${name}!`
